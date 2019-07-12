a28ee00260df5059a4c7ac594e34d59619074159
José María Aguiñaga <jmac04071994@gmail.com>
2019-07-12 12:39:30 -0500

    Agregar archivos de proyecto.

---
+++ ITI901_ProyectoTutorial_Aguinaga/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ITI901_ProyectoTutorial_Aguinaga
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ ITI901_ProyectoTutorial_Aguinaga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITI901_ProyectoTutorial_Aguinaga.Startup))]
+namespace ITI901_ProyectoTutorial_Aguinaga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
